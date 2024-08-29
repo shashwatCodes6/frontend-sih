c7dff36321bf029a4de0ced2deb24626babcf9fa
Rahul Nain <connecttorahulnain@gmail.com>
2024-08-29 22:24:27 +0530

    commit

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Chatbot() {
   return (
-    <div className='absolute bottom-4 right-4'>
+    <div className='fixed bottom-4 right-4'>
     <button className='rounded-full border border-black w-20 h-20' >Chatbot</button>
     </div>
   )
